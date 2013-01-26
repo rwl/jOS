73f05b6e7defe6182b95e579f01038d381ece20c
rwl <r.w.lincoln@gmail.com>
2013-01-26 07:55:04 +0100

    Adding more parameters to maven plug-in.

--- maven-plugin/src/main/java/jos/maven/JOSMojo.java
+++ maven-plugin/src/main/java/jos/maven/JOSMojo.java
@@ -2,27 +2,30 @@ package jos.maven;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
-import jos.j2objc.JOSPlugin;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
+import com.google.common.collect.Lists;
 import com.google.devtools.j2objc.J2ObjC;
-import com.google.devtools.j2objc.Options;
 
-@Mojo(name = "translate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@Mojo(name = "translate", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresDependencyResolution = ResolutionScope.COMPILE)
 public class JOSMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project.compileClasspathElements}", required = true, readonly = true)
+    @SuppressWarnings("rawtypes")
+    private List classpathElements;
 
     @Parameter(defaultValue = "${project.build.sourceDirectory}", required = true)
     private File sourceDirectory;
@@ -33,11 +36,44 @@ public class JOSMojo extends AbstractMojo {
     @Parameter
     private boolean ignoreMissingImports;
 
+    @Parameter
+    private String prefix;
+
+    @Parameter(defaultValue = "false")
+    private boolean verbose;
+
+    @Parameter(defaultValue = "false")
+    private boolean useARC;
+
+    @Parameter(defaultValue = "false")
+    private boolean noPackageDirectories;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        final Collection<File> files = FileUtils.listFiles(sourceDirectory,
-                FileFilterUtils.suffixFileFilter(".java"),
-                DirectoryFileFilter.DIRECTORY);
-        final List<String> args = new ArrayList<String>();
+        final List<String> args = Lists.newArrayList();
+
+        if (ignoreMissingImports) {
+            args.add("--ignore-missing-imports");
+        }
+
+        if (!StringUtils.isEmpty(prefix)) {
+            args.add("--prefix");
+            args.add(prefix);
+        }
+
+        if (verbose) {
+            args.add("-v");
+        }
+
+        if (useARC) {
+            args.add("-use-arc");
+        }
+
+        if (noPackageDirectories) {
+            args.add("--no-package-directories");
+        }
+
+        args.add("-classpath");
+        args.add(StringUtils.join(classpathElements, ":"));
 
         args.add("-d");
         try {
@@ -46,10 +82,9 @@ public class JOSMojo extends AbstractMojo {
             throw new MojoExecutionException(e.getMessage(), e);
         }
 
-        if (ignoreMissingImports) {
-            args.add("--ignore-missing-imports");
-        }
-
+        final Collection<File> files = FileUtils.listFiles(sourceDirectory,
+                FileFilterUtils.suffixFileFilter(".java"),
+                DirectoryFileFilter.DIRECTORY);
         try {
             args.add(outputDirectory.getCanonicalPath());
             for (final File file : files) {
@@ -58,8 +93,6 @@ public class JOSMojo extends AbstractMojo {
         } catch (final IOException e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
-
-        Options.getPlugins().add(new JOSPlugin());
 
         J2ObjC.main(args.toArray(new String[args.size()]));
     }
--- samples/hello-world/src/main/java/jos/samples/hello/Translate.java
+++ samples/hello-world/src/main/java/jos/samples/hello/Translate.java
@@ -5,12 +5,11 @@ import com.google.devtools.j2objc.J2ObjC;
 public class Translate {
 
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.class.path"));
-
-        J2ObjC.main(new String[] { //"--prefix", "jos.samples.hello=J",
-                //"-use-arc",
-                "-sourcepath",
-                "/home/rwl/java/jOS/jos/samples/hello-world/src/main/java",
+        J2ObjC.main(new String[] {
+                "--prefix", "jos.samples.hello=J",
+                "-v",
+                "-use-arc",
+                "-sourcepath", "src/main/java",
                 "-classpath", System.getProperty("java.class.path"),
                 "src/main/java/jos/samples/hello/AppDelegate.java",
                 "src/main/java/jos/samples/hello/HelloViewController.java",

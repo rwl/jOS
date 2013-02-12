8c2b09d5d4ef2f3c6f3355877433bf4021b23b8b
rwl <r.w.lincoln@gmail.com>
2013-02-13 00:02:23 +0100

    Removing EventListener registration.

--- samples/standard-controls/src/main/java/jos/samples/controls/controls/EventListener.java
+++ samples/standard-controls/src/main/java/jos/samples/controls/controls/EventListener.java
@@ -1,14 +1,13 @@
 package jos.samples.controls.controls;
 
-import com.google.j2objc.annotations.Export;
-import com.google.j2objc.annotations.Register;
-import com.google.j2objc.annotations.Target;
-
 import jos.api.foundation.NSObject;
 import jos.api.uikit.UIEvent;
 
+import com.google.j2objc.annotations.Export;
+import com.google.j2objc.annotations.Target;
+
 @Target("onEvent:event:")
-@Register(isWrapper = true)
+//@Register(isWrapper = true)
 public interface EventListener {
 
     @Export("onEvent:event:")
